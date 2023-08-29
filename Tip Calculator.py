50732f6e52083fd73ed5257d67f412ff891c88fb
Jesse Ntow <Jessentow@gmail.com>
2023-08-29 09:46:36 +0000

    First Commit

---
+++ Tip Calculator.py
@@ -0,0 +1,25 @@
+charge_of_food = input("Please enter the charge of food : ")
+print()
+
+x = charge_of_food
+print("Charge of Food is = $"+x)
+print()
+y= 18/100
+r = float(x)
+z = y*r
+
+
+e = round(z, 2)
+
+print ("Tips = $" +str(e))
+print()
+
+a = 7/100
+b = a*r
+s= round(b, 2)
+print("Sales Tax = $"+str (s))
+
+print()
+
+k = s+e+r
+print("Grand Total = $"+str(k))
